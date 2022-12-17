a2c01c79a14091e8ca1d7e76b07ec35a6af6f5e5
tiago <canjhagamey@gmail.com>
2022-12-17 16:22:21 -0300

    Upcarousel

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
-let rellax = new Rellax('.rellax');
+const myCarousel = new Carousel(document.querySelector(".carousel"), {
+    slides: [
+        {
+            html: "1",
+        },
+        {
+            html: "2",
+        },
+        {
+            html: "3",
+        },
+    ],
+});
