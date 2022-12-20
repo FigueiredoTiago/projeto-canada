086bace73e78b5b935fed17c65e78db7cd8ff15d
tiago <canjhagamey@gmail.com>
2022-12-20 18:59:20 -0300

    update

--- code.js
+++ code.js
@@ -1,13 +1,25 @@
-const myCarousel = new Carousel(document.querySelector(".carousel"), {
-    slides: [
-        {
-            html: "1",
-        },
-        {
-            html: "2",
-        },
-        {
-            html: "3",
-        },
-    ],
+//menu responsivo
+const nav = document.getElementById('nav');
+const btn = document.getElementById('btn-mobile');
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    if (nav.classList == 'active') {
+        document.getElementById('btn-mobile').src = "img/icons/x.png"
+    } else {
+        document.getElementById('btn-mobile').src = "img/icons/menu.png"
+    }
+});
+
+//carousel
+const swiper = new Swiper(".mySwiper", {
+    effect: "flip",
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
